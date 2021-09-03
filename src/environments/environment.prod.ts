9b2d87db154ba672f9f793acd42b63d7df17f999
Federico Tocco <fedecap26@gmail.com>
2021-09-02 23:58:57 -0300

    Añadido credenciales firebase a archivo production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCFwVkAXtJ7SxvZSOeSlEM4kHR02kMXBRo',
+    authDomain: 'angularutn-ed18b.firebaseapp.com',
+    projectId: 'angularutn-ed18b',
+    storageBucket: 'angularutn-ed18b.appspot.com',
+    messagingSenderId: '182835672731',
+    appId: '1:182835672731:web:1731f670a3aecf89ce783e',
+    measurementId: 'G-PKKF9H5VEC',
+  },
 };
